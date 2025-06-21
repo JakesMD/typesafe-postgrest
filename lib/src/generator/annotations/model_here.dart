f31bfb8d70da38c9a8cc8f39340c3f4a57fd5f91
JakesMD <71591438+JakesMD@users.noreply.github.com>
2025-06-21 16:10:41 +0200

    📝 Add missing documentation comments

--- lib/src/generator/annotations/model_here.dart
+++ lib/src/generator/annotations/model_here.dart
@@ -1,3 +1,16 @@
+import 'package:typesafe_postgrest/typesafe_postgrest.dart';
+
+/// {@template typesafe_postgrest.PgModelHere}
+///
+/// An annotation that marks a class as a [PgModel] for which an extension
+/// should be generated.
+///
+/// The extension will contain the getter methods for the model's properties.
+///
+/// The annotated class must have a static [PgModelBuilder] field.
+///
+/// {@endtemplate}
 class PgModelHere {
+  /// {@macro typesafe_postgrest.PgModelHere}
   const PgModelHere();
 }
--- lib/src/generator/builder.dart
+++ lib/src/generator/builder.dart
@@ -2,5 +2,6 @@ import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:typesafe_postgrest/src/generator/model_x_generator.dart';
 
+/// The builder for the [PgModelXGenerator].
 Builder pgModelXBuilder(BuilderOptions options) =>
     SharedPartBuilder([PgModelXGenerator()], 'model_x');
