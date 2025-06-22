106af38940e7a79c54b3ae319797730edb996bb0
JakesMD <71591438+JakesMD@users.noreply.github.com>
2025-06-22 20:15:39 +0200

    📝 Add missing documentation comments

--- lib/src/model/nullable.dart
+++ lib/src/model/nullable.dart
@@ -1,5 +1,15 @@
+/// {@template typesafe_postgrest.PgNullable}
+///
+/// A nullable value.
+///
+/// This allows Dart to distinguish between an optional parameter and a value
+/// set to null.
+///
+/// {@endtemplate}
 class PgNullable<T> {
+  /// {@macro typesafe_postgrest.PgNullable}
   const PgNullable(this.value);
 
+  /// The value that may be null but is not an optional parameter.
   final T? value;
 }
